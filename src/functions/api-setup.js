1f30fff56c53b7bf175ce7c563f767388dbf6819
Bassem <bassem@MacBook-Pro-de-Bassem.local>
2022-05-12 23:11:16 -0300

    use api fetch to post and get data

---
+++ src/functions/api-setup.js
@@ -0,0 +1,33 @@
+const api =
+  "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/UEH7RC3HNQw5hcmpfDEd/scores/";
+
+const scoreShow = (players) => {
+  const scoreSheet = document.querySelector(".high-list");
+  scoreSheet.innerHTML = ''
+  players.forEach((player) => {
+    scoreSheet.innerHTML += `<li class="high-score">${player.user} : ${player.score}</li>`;
+  });
+};
+
+async function getData() {
+  const stored = await fetch(api);
+  const data = stored.json();
+  return data;
+}
+
+const insertPlayer = async () => {
+  const players = await getData();
+  scoreShow(players.result);
+};
+
+const addUser = async (data) => {
+  await fetch(api, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ user: data[0], score: data[1] }),
+  });
+};
+
+export { addUser, getData, insertPlayer };
--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
 // import _ from "lodash";
-import './styles.css';
+import { addUser, insertPlayer } from "./functions/api-setup.js";
+import "./styles.css";
+
+const refreshButton = document.querySelector(".refresh");
+const addForm = document.querySelector("form");
+
+addForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const player = document.querySelector(".name");
+  const score = document.querySelector("#scored");
+  await addUser([player.value, score.value]);
+  insertPlayer();
+  e.target.reset();
+});
+
+window.onload = () => {
+  insertPlayer();
+};
+
+refreshButton.addEventListener('click', () => {
+  insertPlayer();
+});
